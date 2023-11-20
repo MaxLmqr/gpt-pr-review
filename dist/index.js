07e7a67df5464dfa646de856b8a1a9bbc3766d90
MaxLmqr <max@galadrim.fr>
2023-11-20 19:06:00 +0100

    chore: update prompt

--- dist/index.js
+++ dist/index.js
@@ -33475,13 +33475,14 @@ exports.run = run;
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.systemContent = exports.baseContent = void 0;
+exports.systemContent = exports.baseContentGPT = exports.baseContent = void 0;
 exports.baseContent = `You will receive a GitHub patch file content.
-Assume that the code is not breaking anything. Ignore the implication of the code outside of the file. Do not comment with compliment about the code.
+Assume that the code is not breaking anything. Assume the developer is experienced and knows what they're doing.
 Rate the code from 1 to 100. 1 being the worst and 100 the best. Comment with the rate.
 Do NOT explain what the code is doing. if the file is not a code file, answer "No comment".
-Do not make assumption, only state facts about the code.
+Please focus on code style, readability, and adherence to best practices. Do not consider external dependencies or future implications of the code change.
 If the rate is equal or above 80, answer with "No comment". Else, review the code : \n`;
+exports.baseContentGPT = `Please review the following code snippet, assuming the developer is experienced and knowledgeable. Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. Do not explain what the code is doing functionally. Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide a brief comment on areas of improvement if the rating is below 80; otherwise, respond with 'No comment.`;
 exports.systemContent = `You are a lead developer and you are reviewing a pull request from a junior developer.`;
 
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,10 @@
 export const baseContent = `You will receive a GitHub patch file content.
-Assume that the code is not breaking anything. Ignore the implication of the code outside of the file. Do not comment with compliment about the code.
+Assume that the code is not breaking anything. Assume the developer is experienced and knows what they're doing.
 Rate the code from 1 to 100. 1 being the worst and 100 the best. Comment with the rate.
 Do NOT explain what the code is doing. if the file is not a code file, answer "No comment".
-Do not make assumption, only state facts about the code.
+Please focus on code style, readability, and adherence to best practices. Do not consider external dependencies or future implications of the code change.
 If the rate is equal or above 80, answer with "No comment". Else, review the code : \n`
+
+export const baseContentGPT = `Please review the following code snippet, assuming the developer is experienced and knowledgeable. Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. Do not explain what the code is doing functionally. Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide a brief comment on areas of improvement if the rating is below 80; otherwise, respond with 'No comment.`
 
 export const systemContent = `You are a lead developer and you are reviewing a pull request from a junior developer.`
