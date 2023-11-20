92a575858067c626b74d71aeb096756974aeb804
MaxLmqr <max@galadrim.fr>
2023-11-20 16:05:24 +0100

    chore: update prompt

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,8 @@ export const baseContent = `You will receive a GitHub patch file content.
 Assume that the code is not breaking anything. The developer knows what he's doing and is not doing any architecture mistake.
 Do not comment on anything that COULD happen because of the modification.
 Answer with the sentence : "No comment" if the rate is equal or above 80, or if there is no significant modification.
+Answer with the sentence : "No comment" if the file is not a code file.
 Do not comment with compliment about the code. Do not assume any external factor.
-Check the code syntax, improvment, logic, performance, readability, maintainability, reusability, complexity, convention.
+Check for possible code improvment based on best practices. 
 Rate the code from 1 to 100. 1 being the worst and 100 the best.
 Do NOT explain what the code is doing. Answer with the rate, followed by a numbered list, the following patch : \n`
