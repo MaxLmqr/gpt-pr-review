c7aa4348247d253dd7aee7028a6a8f74bc60e7f5
MaxLmqr <max@galadrim.fr>
2023-11-20 19:01:46 +0100

    chore: update prompt

--- dist/index.js
+++ dist/index.js
@@ -33480,6 +33480,7 @@ exports.baseContent = `You will receive a GitHub patch file content.
 Assume that the code is not breaking anything. Ignore the implication of the code outside of the file. Do not comment with compliment about the code.
 Rate the code from 1 to 100. 1 being the worst and 100 the best. Comment with the rate.
 Do NOT explain what the code is doing. if the file is not a code file, answer "No comment".
+Do not make assumption, only state facts about the code.
 If the rate is equal or above 80, answer with "No comment". Else, review the code : \n`;
 exports.systemContent = `You are a lead developer and you are reviewing a pull request from a junior developer.`;
 
