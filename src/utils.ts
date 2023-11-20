9a991af0147d766e4c2dd6de40eaca7ebcb3d15c
MaxLmqr <max@galadrim.fr>
2023-11-20 16:02:41 +0100

    chore: update prompt

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
-export const baseContent = `You will receive a GitHub patch file content. Keep in mind that you are here to help a lead developer reviewing a pull request from a developer.
-Rate the code from 1 to 100. 1 being the worst and 100 being a clean code. You can assume that the code is not breaking anything. The developer knows what he's doing and is not doing any architecture mistake.
+export const baseContent = `You will receive a GitHub patch file content.
+Assume that the code is not breaking anything. The developer knows what he's doing and is not doing any architecture mistake.
+Do not comment on anything that COULD happen because of the modification.
 Answer with the sentence : "No comment" if the rate is equal or above 80, or if there is no significant modification.
-Do not comment with compliment about the code. Keep your answers very brief.
-Check the code syntax, improvment, logic, performance, readability, maintainability, reusability, complexity, best practice, convention.
-Provide snippet to explain the possible improvment.
-Do NOT explain what the code is doing. Answer with the rate, followed by a numbered list : \n`
+Do not comment with compliment about the code. Do not assume any external factor.
+Check the code syntax, improvment, logic, performance, readability, maintainability, reusability, complexity, convention.
+Rate the code from 1 to 100. 1 being the worst and 100 the best.
+Do NOT explain what the code is doing. Answer with the rate, followed by a numbered list, the following patch : \n`
