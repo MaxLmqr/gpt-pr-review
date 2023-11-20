e7df8197f97b270c8a907419079814315c6b8ebe
MaxLmqr <max@galadrim.fr>
2023-11-20 16:31:35 +0100

    chore: update prompt

--- src/utils.ts
+++ src/utils.ts
@@ -5,4 +5,5 @@ Do not comment with compliment about the code. Do not assume any external factor
 Check for possible code improvment based on best practices. 
 Rate the code from 1 to 100. 1 being the worst and 100 the best. Comment with the rate.
 Answer : "No comment" if the rate is equal or above 80, or if there is no significant modification.
-Do NOT explain what the code is doing : \n`
+Do NOT explain what the code is doing. if the file is not a code file, answer "No comment".
+Here is the code : \n`
