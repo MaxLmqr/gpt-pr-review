79c0dd9e4a6dbf1553fecc190f7447b1df569cbf
MaxLmqr <max@galadrim.fr>
2023-11-21 16:47:36 +0100

    chore: update prompt

--- dist/index.js
+++ dist/index.js
@@ -33476,7 +33476,10 @@ exports.run = run;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.systemContent = exports.baseContent = void 0;
-exports.baseContent = `Please review the following GitHub patch file, assuming the developer is experienced and knowledgeable. Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. Do not explain what the code is doing functionally. If the context is insufficient, answer with "No comment". Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide a brief way to improve code, you can use snippet, if the rating is below 80; otherwise, respond with 'No comment'.`;
+exports.baseContent = `Please review the following GitHub patch file, assuming the developer is experienced and knowledgeable. 
+Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. 
+Do not explain what the code is doing functionally. If the context is insufficient, answer with "No comment". 
+Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide snippet to improve code, only if the rating is below 80; otherwise, respond with 'No comment'.`;
 exports.systemContent = `You are a lead developer and you are reviewing a pull request from a developer. Your aim is to point out only the relevant code.`;
 
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
-export const baseContent = `Please review the following GitHub patch file, assuming the developer is experienced and knowledgeable. Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. Do not explain what the code is doing functionally. If the context is insufficient, answer with "No comment". Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide a brief way to improve code, you can use snippet, if the rating is below 80; otherwise, respond with 'No comment'.`
+export const baseContent = `Please review the following GitHub patch file, assuming the developer is experienced and knowledgeable. 
+Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. 
+Do not explain what the code is doing functionally. If the context is insufficient, answer with "No comment". 
+Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide snippet to improve code, only if the rating is below 80; otherwise, respond with 'No comment'.`
 
 export const systemContent = `You are a lead developer and you are reviewing a pull request from a developer. Your aim is to point out only the relevant code.`
