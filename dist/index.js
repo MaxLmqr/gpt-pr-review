eed37b6b7f7f4823a3dbbf958aa7adf0b4899c0e
MaxLmqr <max@galadrim.fr>
2023-11-21 16:52:45 +0100

    chore: update prompt

--- dist/index.js
+++ dist/index.js
@@ -33479,7 +33479,7 @@ exports.systemContent = exports.baseContent = void 0;
 exports.baseContent = `Please review the following GitHub patch file, assuming the developer is experienced and knowledgeable. 
 Focus your evaluation on adherence to coding best practices, readability, and style, without considering the functionality or potential future issues of the code. 
 Do not explain what the code is doing functionally. If the context is insufficient, answer with "No comment". 
-Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. Provide snippet to improve code, only if the rating is below 80; otherwise, respond with 'No comment'.`;
+Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. If the rate is equal or above 80, simply answer "No comment". Otherwise, review with snippet if possible.`;
 exports.systemContent = `You are a lead developer and you are reviewing a pull request from a developer. Your aim is to point out only the relevant code.`;
 
 
