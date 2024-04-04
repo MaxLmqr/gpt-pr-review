ec6119d36ecebd6d11ceaf057d13559c84f89b0c
MaxLmqr <max@galadrim.fr>
2024-04-04 11:44:26 +0200

    fix: add json keyword to base content

--- dist/index.js
+++ dist/index.js
@@ -32849,8 +32849,8 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.systemContent = exports.baseContent = void 0;
 exports.baseContent = `Review GitHub patch file. Focus your evaluation on adherence to coding best practices.
 Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. 
-Answer with a numbered list. Limit your answer to the 5 most important review you found in the code. Each item should be a single concise sentence.
-Use the folowing format : { "score": value, "reviews": [{ line: number of the line where comment should appear in the patch file, message: "content of the comment}, ...]}`;
+Answer with a numbered list. Do not go beyond 5 reviews you found in the code. You can provide less than 5 reviews. Each item should be a single concise sentence.
+Use the folowing json format : { "score": value, "reviews": [{ line: number of the line where comment should appear in the patch file, message: "content of the comment}, ...]}`;
 exports.systemContent = `You are a software engineer reviewing a patch file from a pull request.`;
 
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const baseContent = `Review GitHub patch file. Focus your evaluation on adherence to coding best practices.
 Rate the code on a scale from 1 to 100, where 1 is the worst and 100 is the best. 
-Answer with a numbered list. Limit your answer to the 5 most important review you found in the code. Each item should be a single concise sentence.
-Use the folowing format : { "score": value, "reviews": [{ line: number of the line where comment should appear in the patch file, message: "content of the comment}, ...]}`
+Answer with a numbered list. Do not go beyond 5 reviews you found in the code. You can provide less than 5 reviews. Each item should be a single concise sentence.
+Use the folowing json format : { "score": value, "reviews": [{ line: number of the line where comment should appear in the patch file, message: "content of the comment}, ...]}`
 
 export const systemContent = `You are a software engineer reviewing a patch file from a pull request.`
